60b8bbb5f579e827137437b9cc788ef1dd7d7d90
Vidya Vipul <i.vidyavipul@gmail.com>
2026-03-27 09:04:12 +0530

    chore(infra): align local runtime and docker setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,15 @@
 /// <reference path="../node_modules/nakama-runtime/index.d.ts" />
 // main.ts
-import { matchInit, matchJoinAttempt, matchJoin, matchLeave, matchLoop, matchTerminate, matchSignal } from './tic_tac_toe';
+
+function matchmakerMatched(
+    context: nkruntime.Context,
+    logger: nkruntime.Logger,
+    nk: nkruntime.Nakama,
+    matches: nkruntime.MatchmakerResult[]
+): string {
+    logger.info('Matchmaker matched: %v', matches);
+    return nk.matchCreate('tic_tac_toe', {});
+}
 
 const InitModule: nkruntime.InitModule =
         function(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, initializer: nkruntime.Initializer) {
@@ -18,17 +27,9 @@ const InitModule: nkruntime.InitModule =
 
     // 2. Register the matchmaker matched hook
     // When the matchmaker finds enough players, this hook is triggered.
-    initializer.registerMatchmakerMatched((context: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, matches: nkruntime.MatchmakerResult[]) => {
-        logger.info('Matchmaker matched: %v', matches);
-        
-        // Create a new match on the 'tic_tac_toe' module
-        const matchId = nk.matchCreate('tic_tac_toe', { /* optional params */ });
-        
-        // Return the matchId to the matched players
-        return matchId;
-    });
+    initializer.registerMatchmakerMatched(matchmakerMatched);
 
-    logger.info('Nakama Tic-Tac-Toe module loaded, match and matchmaker registered.');
+    logger.info('XO module loaded, match and matchmaker registered.');
 }
 
 // Global variable that Nakama looks for
